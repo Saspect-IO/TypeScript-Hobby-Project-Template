915d58ee9d5ee4a3e3db28d789bf8ca8844a4390
Sheldon Small <sheldonsmall.io@gmail.com>
2021-07-14 15:02:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# WebGL Procedural Terrain Generator
+
+project description.
+
+
+## 🚀 Demo 
+- [Demo](https://saspect-io.github.io/)
+
+
+## Dev Usage
+
+- Clone or download repo
+- npm install
+- npm run dev
+
+
+## Deployment
+
+- npm run build
+
+
+## Resources for learning
+
+- [link](https://saspect-io.github.io/)
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ROOT = path.resolve(__dirname, 'src');
+
+module.exports = {
+
+  devtool: 'source-map',
+
+  // bundling mode
+  mode: 'production',
+
+  // entry files
+  entry: './src/main.ts',
+
+  // output bundles (location)
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  // file resolutions
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    },
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    modules: [
+      ROOT,
+      'node_modules'
+    ]
+  },
+
+  // loaders
+  module: {
+    rules: [{
+        test: /\.tsx?/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          }
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'assets/resources',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|obj)$/i,
+        type: 'assets/resources',
+      },
+    ]
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+};
